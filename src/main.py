727a9ce6f3839d9fa5865f2725fd2c0d0a3d9a3e
Yining Zhang <602799027@qq.com>
2020-06-19 16:05:29 +0800

    Correction nonstandard writing in main

--- src/main.py
+++ src/main.py
@@ -12,16 +12,16 @@ os.environ['ANDROID_HOME'] = android_home
 if __name__ == "__main__":
     accountList = getAccounts()
 
-    g = game()
-    g.Dungeon()
-    g.taskbonus()
-    g.gift()
-    g.backToTitle()
-    del g
-    os.system(".\\..\\platform-tools\\adb.exe kill-server")
-    time.sleep(10)
+    # g = game()
+    # g.Dungeon()
+    # g.taskbonus()
+    # g.gift()
+    # g.backToTitle()
+    # del g
+    # os.system(".\\..\\platform-tools\\adb.exe kill-server")
+    # time.sleep(10)
     
-    for account in accountList[7:]:
+    for account in accountList[:]:
         g = game()
         g.login(account)
         g.taskbonus()
