cb0e15ba20dc5ba9bba90ea179d253a7cdff3807
Yining Zhang <602799027@qq.com>
2020-06-21 14:37:08 +0800

    update README and add donate

--- src/main.py
+++ src/main.py
@@ -11,17 +11,7 @@ os.environ['ANDROID_HOME'] = android_home
 
 if __name__ == "__main__":
     accountList = getAccounts()
-
-    # g = game()
-    # g.Dungeon()
-    # g.taskbonus()
-    # g.gift()
-    # g.backToTitle()
-    # del g
-    # os.system(".\\..\\platform-tools\\adb.exe kill-server")
-    # time.sleep(10)
     os.system(".\\..\\platform-tools\\adb.exe kill-server")
-    
     for account in accountList[:]:
         g = game()
         g.login(account)
